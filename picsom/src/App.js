b4b36eb04d6f940533adc3e7caf47a51b12f8b54
RyanSyme <ryu321@hotmail.com>
2021-11-03 13:54:01 +0000

    add react-router, links and switch

--- picsom/src/App.js
+++ picsom/src/App.js
@@ -1,15 +1,25 @@
 import React from "react"
-// import Header from "./components/Header"
-// import Cart from "./pages/Cart"
-// import Photos from "./pages/Photos"
+import {Switch, Route} from "react-router-dom"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
 
 function App() {    
     return (
         <div>
-            {/* <Header /> */}
-            <h1>Home Page</h1>
+            <Header />
+            <Switch>
+                <Route exact path="/">
+                    <Photos/>
+                </Route>
+                <Route Path="/cart">
+                    <Cart/>
+                </Route>
+            </Switch>
         </div>
     )
+
+
 }
 
 export default App
