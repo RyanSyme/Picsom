96c9162ec94d93e429b628aa9c25c9de68210d2f
RyanSyme <ryu321@hotmail.com>
2021-11-09 12:11:49 +0000

    add toggleFavorite to context.js

--- picsom/src/components/Image.js
+++ picsom/src/components/Image.js
@@ -1,8 +1,14 @@
-import React, {useState} from "react"
+import React, {useState, useContext} from "react"
+import {Context} from "../Context"
 
 function Image({className, img}) {
     const [hovered, setHovered] = useState(false)
-    console.log(hovered)
+    const {toggleFavorite} = useContext(Context)
+
+const heartIcon = hovered && 
+    <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
     return (
         <div 
             className={`${className} image-container`}
@@ -10,6 +16,8 @@ function Image({className, img}) {
             onMouseLeave={() => setHovered(false)}
         >
             <img src={img.url} className="image-grid"/>
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
