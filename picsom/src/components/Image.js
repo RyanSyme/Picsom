392fa62711e946bf65306081b94e989f5b634bb8
RyanSyme <ryu321@hotmail.com>
2021-11-09 13:21:00 +0000

    add proptypes

--- picsom/src/components/Image.js
+++ picsom/src/components/Image.js
@@ -1,4 +1,6 @@
 import React, {useState, useContext} from "react"
+import PropTypes from "prop-types"
+
 import {Context} from "../Context"
 
 function Image({className, img}) {
@@ -28,6 +30,15 @@ const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
             {cartIcon}
         </div>
     )
+}
+
+Image.propTypes = {
+    className: PropTypes.string,
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    })
 }
 
 export default Image
