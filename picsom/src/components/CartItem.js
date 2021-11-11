3a02cd95313bbc6ffc47dd62c19c0c24f2af4c9a
RyanSyme <ryu321@hotmail.com>
2021-11-11 15:56:50 +0000

    Revert "minor fixes to useHooks.js, remove unused useState from imports"
    This reverts commit 398f2fdac83e3fcd0193d378411c1088b58a9a0e.

--- picsom/src/components/CartItem.js
+++ picsom/src/components/CartItem.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react"
+import React, {useState, useContext} from "react"
 import PropTypes from "prop-types"
 import {Context} from "../Context"
 import useHover from "../hooks/useHover"
